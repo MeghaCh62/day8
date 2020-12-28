5a4deecc9fc1bb21ce9ce62a5d166d5457fb3f80
Megha <megha.maheswari@terralogic.com>
2020-12-28 15:41:37 +0530

    day8

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+//import img1 from './images/img1.jpg';
+
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+            
+            <div className="img1">
+           
+            <center><h1>Home Page</h1></center>
+           
+
+            </div>
+</div>  
+               
+          
+        );
+    }
+}
+export default home;
